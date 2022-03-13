a95f0479043e69b66bdfd48674735b6626b3431e
Zo <zoe.marday@outlook.com>
2022-03-13 14:53:50 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,52 @@ import { DetailsProductComponent } from "./details-product/details-product.compo
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ManageStockComponent } from "./manage-stock/manage-stock.component";
 
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'detailsProduit', component: DetailsProductComponent },
-  { path: 'productList', component: ProductListComponent},
-  { path: 'manageStock', component: ManageStockComponent},
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'detailsProduit',
+    component: DetailsProductComponent
+  },
+  {
+    path: 'productList',
+    component: ProductListComponent
+  },
+  {
+    path: 'manageStock',
+    component: ManageStockComponent
+  },
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('.layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('.layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
+      }
+    ]
+  },
 
 ]
 
@@ -22,10 +60,10 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
   ],
-  exports: [
-    RouterModule
-  ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DetailsProductComponent } from './details-product/details-product.component';
 import { FormsModule } from "@angular/forms";
 import { ManageStockComponent } from './manage-stock/manage-stock.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
 @NgModule({
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   declarations: [
     AppComponent,
     HomeComponent,
@@ -18,14 +25,10 @@ import { ProductListComponent } from './product-list/product-list.component';
     DetailsProductComponent,
     ManageStockComponent,
     ProductListComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AdminLayoutComponent,
+    AuthLayoutComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
